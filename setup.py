bb132df7b7d72d6c33f8bea5f910d0b5e3d0b8df
PBeGood4 <82064173+PBeGood4@users.noreply.github.com>
2021-05-04 17:20:13 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["pynput>=1.7.3"]
+
+setup(
+    name="cookie_clicker-bot",
+    version="1",
+    author="PBeGood4",
+    author_email="pbegood.mail@gmail.com",
+    description="Fastest cookie clicker bot in the west.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PBeGood4/cookie_clicker-bot/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)

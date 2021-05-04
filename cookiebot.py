2f0fd9847c4454918267f26c6c63115dce667242
PBeGood4 <82064173+PBeGood4@users.noreply.github.com>
2021-05-04 17:25:23 +0100

    Add files via upload

---
+++ cookiebot.py
@@ -0,0 +1,60 @@
+import time
+import threading
+import pynput
+import sys
+
+from pynput import mouse,keyboard
+from pynput.mouse import Controller, Button
+
+time.sleep(1)
+print("Starting in 5")
+time.sleep(1)
+print("Starting in 4")
+time.sleep(1)
+print("Starting in 3")
+time.sleep(1)
+print("Starting in 2")
+time.sleep(1)
+print("Starting in 1")
+time.sleep(1)
+print("Click Click Click Click Click...")
+time.sleep(1)
+print("Press ESC to exit")
+
+a=False
+
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    global a
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        a=True
+        return False
+
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+def listen():
+    listener.join()
+
+threading.Thread(target=listen).start()
+
+mouse = Controller()
+
+def func1():
+    while True:
+        if a:
+            break
+        mouse.click(Button.left)
+        time.sleep(0.0007)
+
+func1()
